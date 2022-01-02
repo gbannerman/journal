e81f7472ff91759f041aeb9fb3bb7549aba90f19
Gavin Bannerman <16233190+gbannerman@users.noreply.github.com>
2022-01-02 20:31:31 +0000

    Fix importDiary by uncommenting `put`

--- importDiary/db.js
+++ importDiary/db.js
@@ -1,5 +1,5 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const { DynamoDBDocumentClient, PutCommand  } = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
 
 const client = new DynamoDBClient({ region: "eu-north-1" });
 const documentClient = DynamoDBDocumentClient.from(client);
@@ -7,21 +7,21 @@ const documentClient = DynamoDBDocumentClient.from(client);
 const UPDATED_TABLE_NAME = "TravelDiary";
 
 const putItem = async (item) => {
-    const params = {
-        TableName: UPDATED_TABLE_NAME,
-        Item: item,
-    }
+  const params = {
+    TableName: UPDATED_TABLE_NAME,
+    Item: item,
+  };
 
-    const putItem = new PutCommand(params);
+  const putItem = new PutCommand(params);
 
-    try {
-        console.log(item);
-        //return await documentClient.send(putItem);
-    } catch (err) {
-        console.error(err);
-    }
-}
+  try {
+    console.log(item);
+    return await documentClient.send(putItem);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = {
-    putItem,
-}
+  putItem,
+};
