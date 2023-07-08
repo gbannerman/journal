1a74828568a7d7c473364a2d0267bc7cbff62960
Gavin Bannerman <16233190+gbannerman@users.noreply.github.com>
2023-07-08 17:58:17 +0100

    Improve logging of checkForTravelDiaryEntry

--- checkForTravelDiaryEntry/index.js
+++ checkForTravelDiaryEntry/index.js
@@ -1,56 +1,67 @@
 const { Lambda } = require("@aws-sdk/client-lambda");
-const { DateTime } = require('luxon');
-const { getEntryForDate } = require('./diary');
+const { DateTime } = require("luxon");
+const { getEntryForDate } = require("./diary");
 
-exports.handler = async (event) => {
-    const lambdaClient = new Lambda({ region: "eu-north-1" });
+exports.handler = async () => {
+  const lambdaClient = new Lambda({ region: "eu-north-1" });
 
-    const day = DateTime.now();
+  const day = DateTime.now();
 
-    console.log(`Getting entries for ${day.toISODate()}`);
+  console.log(`Getting entries for ${day.toISODate()}`);
 
-    const entries = await getEntryForDate(day);
+  const entries = await getEntryForDate(day);
 
-    const formattedDay = day.toFormat("d MMMM");
+  const formattedDay = day.toFormat("d MMMM");
 
-    const formattedEntries = entries.map(x => ({
-        ...x,
-        yearsAgo: parseInt(day.toFormat("yyyy")) - x.year,
-    }))
+  const formattedEntries = entries.map((x) => ({
+    ...x,
+    yearsAgo: parseInt(day.toFormat("yyyy")) - x.year,
+  }));
 
-    if (entries.length) {
-        const checkPhotosParams = formattedEntries.map(x => ({
-            FunctionName: "checkPhotos",
-            InvocationType: "RequestResponse",
-            Payload: JSON.stringify({ date: `${x.year}-${x.day}` }),
-        }))
+  if (entries.length) {
+    const checkPhotosParams = formattedEntries.map((x) => ({
+      FunctionName: "checkPhotos",
+      InvocationType: "RequestResponse",
+      Payload: JSON.stringify({ date: `${x.year}-${x.day}` }),
+    }));
 
-        const lambdaResults = await Promise.all(checkPhotosParams.map(x => lambdaClient.invoke(x)));
+    const lambdaResults = await Promise.all(
+      checkPhotosParams.map((x) => lambdaClient.invoke(x))
+    );
 
-        const results = lambdaResults.map(({ Payload }) => JSON.parse(Buffer.from(Payload)));
+    const results = lambdaResults.map(
+      ({ Payload, FunctionError, StatusCode }, index) => {
+        console.info(
+          checkPhotosParams[index].Payload,
+          StatusCode,
+          FunctionError,
+          Buffer.from(Payload).toString()
+        );
+        return JSON.parse(Buffer.from(Payload));
+      }
+    );
 
-        console.log(results);
-        
-        results.forEach((x, i) => {
-            if (x.url) {
-                formattedEntries[i].url = x.url;
-            }
-        });
+    console.log(results);
 
-        const params = {
-            FunctionName: "sendOnThisDayEmail",
-            InvocationType: "Event",
-            Payload: JSON.stringify({ formattedEntries, formattedDay }),
-        };
+    results.forEach((x, i) => {
+      if (x.url) {
+        formattedEntries[i].url = x.url;
+      }
+    });
 
-        await lambdaClient.invoke(params);
-    }
-
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(formattedEntries),
+    const params = {
+      FunctionName: "sendOnThisDayEmail",
+      InvocationType: "Event",
+      Payload: JSON.stringify({ formattedEntries, formattedDay }),
     };
 
-    return response;
-};
+    await lambdaClient.invoke(params);
+  }
 
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(formattedEntries),
+  };
+
+  return response;
+};
